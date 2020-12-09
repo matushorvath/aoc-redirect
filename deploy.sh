99e0605361df4e0ccceeb3b985a5691ac17d05f8
Matus Horvath <matus.horvath@gmail.com>
2020-12-09 19:07:05 +0100

    Dynamo write works

--- deploy.sh
+++ deploy.sh
@@ -12,11 +12,11 @@ bucket=cf.009116496185.us-east-1
 zip -rq $package $(<package.json jq -re .files[],.deployFiles[])
 
 aws s3 cp \
-    --profile private --region us-east-1 \
+    --region us-east-1 \
     $package s3://$bucket/$package
 
 aws cloudformation deploy \
-    --profile private --region us-east-1 \
+    --region us-east-1 \
     --capabilities CAPABILITY_IAM \
     --template template.yml --stack-name aoc-redir \
     --parameter-overrides \
--- main.js
+++ main.js
@@ -2,24 +2,47 @@
 
 const express = require('express');
 const ase = require('aws-serverless-express');
+const aws = require('aws-sdk');
 const nocache = require('nocache');
+const { v4: uuidv4 } = require('uuid');
 
-const redirect = (req, res) => {
-    res.redirect('https://www.google.com');
+const db = new aws.DynamoDB({ apiVersion: '2012-08-10' });
+const dbTable = 'aoc-redirect';
+
+const redirect = async (req, res) => {
+    if (!req.query.name) {
+        res.status(400);
+        res.send(`usage: https://${req.hostname}/2020/day/8?name=dedojozef`);
+        return;
+    }
+
+    const params = {
+        Item: {
+            "day": { S: req.params.day },
+            "name": { S: req.query.name },
+            "time": { S: new Date().toISOString() },
+            "uuid": { S: uuidv4() },
+            "year": { N: req.params.year }
+        },
+        TableName: dbTable
+    };
+    await db.putItem(params).promise();
+
+    res.redirect(`https://adventofcode.com/${req.params.year}/day/${req.params.day}`);
 };
 
 const app = express();
 
 app.use(nocache());
-app.get('/', redirect);
-app.get('/abc', (req, res) => res.send("hello!"));
+app.get('/:year/day/:day', redirect);
+app.get('/data', (req, res) => res.send("hello!"));
 
-if (process.env.IN_LAMBDA === 'true') {
+if (process.env.AWS_EXECUTION_ENV) {
     const server = ase.createServer(app, () => console.log('Server is listening'));
 
     exports.handler = async (event, context) => {
         try {
-            return ase.proxy(server, event, context, 'PROMISE').promise;
+            return await ase.proxy(server, event, context, 'PROMISE').promise;
         } catch (error) {
             console.debug('Internal server error:', error);
             return {
