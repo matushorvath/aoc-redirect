6d90434b654d1de6ca4a3e50345aba3094b0dbe9
Matus Horvath <matus.horvath@gmail.com>
2021-10-10 19:37:04 +0200

    Redeploy to eu-central-1

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,16 @@ set -e
 version=$(<package.json jq -re .version)
 uuid=$(uuidgen -r | tr -d -)
 package=aoc-redirect-$version-$uuid.zip
-bucket=cf.009116496185.us-east-1
+bucket=cf.009116496185.eu-central-1
 
 zip -rq $package $(<package.json jq -re .files[],.deployFiles[])
 
 aws s3 cp \
-    --region us-east-1 \
+    --region eu-central-1 \
     $package s3://$bucket/$package
 
 aws cloudformation deploy \
-    --region us-east-1 \
+    --region eu-central-1 \
     --capabilities CAPABILITY_IAM \
     --template template.yml --stack-name aoc-redir \
     --parameter-overrides \
